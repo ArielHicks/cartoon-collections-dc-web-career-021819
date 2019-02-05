b6e149c6674ecaab4bb5ef1de6a64c6cc7ed79de
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-05 18:01:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  cheese_types.first
+  cheese_types.map {|cheese| puts "#{[0]}"}
 end
