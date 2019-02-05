5feedd436e64a44293d72127035b7b35e408cd90
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-05 17:50:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def summon_captain_planet(array)
   planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  array.length <= 4
   # Your code here
 end
 
