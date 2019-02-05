f5b3d600e8f961e56d498e7ffecc90f4b00f6dc3
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-05 18:00:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,6 @@ def long_planeteer_calls(array)
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+
+  cheese_types.first
 end
